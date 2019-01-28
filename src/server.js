c1f9c84d131f1ca37b73078538ac09e8a156cdd9
laschuet <laschuet@users.noreply.github.com>
2019-01-28 20:38:24 +0100

    Implement ping pong

--- src/server.js
+++ src/server.js
@@ -1,20 +1,44 @@
 import WebSocket from 'ws';
 
+/**/
+function handleClose() {}
+
+/**/
+function handleError(error) {
+  console.error(error.message);
+}
+
+/**/
+function handleMessage(message) {
+  console.log(message);
+}
+
+/**/
+function handlePong() {
+  this.isAlive = true;
+}
+
 const wss = new WebSocket.Server({ port: 8085 });
 
 wss.on('connection', ws => {
-  /**/
-  ws.on('message', message => {
-    console.log(message);
-  });
+  ws.isAlive = true;
 
-  /**/
-  ws.on('close', () => {});
-
-  /**/
-  ws.on('error', error => {
-    console.error(error.message);
-  });
+  ws.on('close', handleClose);
+  ws.on('error', handleError);
+  ws.on('message', handleMessage);
+  ws.on('pong', handlePong);
 
   ws.send('WebSocket server is up');
 });
+
+setInterval(() => {
+  wss.clients.forEach(ws => {
+    if (ws.isAlive === false) {
+      ws.terminate();
+      return;
+    }
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
